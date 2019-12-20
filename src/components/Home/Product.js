8f45c55bb5c0ee92e57c6f920a97d50046deef53
ricardomcc7 <codingdata7@gmail.com>
2019-12-20 03:21:18 -0600

    Revert "Testing cart 1"
    This reverts commit 16158b308de9939a069bd5053a2a757c771ecd2f.

--- src/components/Home/Product.js
+++ src/components/Home/Product.js
@@ -9,14 +9,13 @@ export default function Product({ product }) {
           <div className="card-body text-center">
             <h6>{product.title}</h6>
             <h6>${product.price}</h6>
-
             <button
               className="btn btn-yellow mt-3 text-capitalize snipcart-add-item"
               data-item-id={product.id}
               data-item-name={product.title}
               data-item-price={product.price}
               data-item-image={product.image.fluid.src}
-              data-item-url="https://gatsby-coffee-shop1.netlify.com/"
+              data-item-url="/"
             >
               add to cart
             </button>
